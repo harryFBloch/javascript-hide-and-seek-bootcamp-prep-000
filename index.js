de333d782eac5db5c19d79587e6a969c7daf0edd
Harry Bloch <harryfbloch@gmail.com>
2018-08-29 19:02:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(traget, nested){
+  return document.getElementById(nested).querySelector(target);
+}
+
+function increaseRankby(n) {
+  rankedLists = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < rankedLists.length; i++){
+    let list = rankedLists[i].getElementsByTagName("li");
+    for (let z = 0; z < list.length; z++){
+      list[z].innerText  = parseInt(list[z].innerText) + n
+    }
+  }
+}
+
+function deepestChild(){
+  let parrent = document.getElementsByClassName("grand-node")
+  let searchQue = []
+}
